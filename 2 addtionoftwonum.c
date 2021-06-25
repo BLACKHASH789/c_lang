f75d93a8969d55fa08f1abea0e7e2b6fc098aac4
BLACKHASH789 <sanketbhujade1@gmail.com>
2021-06-26 00:33:48 +0530

    first commit

---
+++ 2 addtionoftwonum.c
@@ -0,0 +1,11 @@
+// adding two numbers 
+
+#include<stdio.h>
+void main()
+{
+   int a,b,c=0;
+    printf("enter two numbers");
+
+    scanf("%d%d",&a,&b);
+    printf("addition is %d",a+b);
+}
---
+++ 3 areaofrectangle.c
@@ -0,0 +1,17 @@
+/*        */
+
+#include<stdio.h>
+int main()
+{
+    int breadth,length;
+    
+
+    printf("enter length");
+    scanf("%d",&length);
+    printf("enter breadth");
+    scanf("%d",&breadth); 
+    printf("area of rectangle is %d ",breadth*length);
+
+
+    return 0;
+}
